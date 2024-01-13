85439e49ce1d1cb738fc403b129b8f74d315b351
Dimitar Dimitrov <mitko.69@gmail.com>
2024-01-13 16:13:15 +0000

    #3 Update Apache log configuration and index.php content

--- www/index.php
+++ www/index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World!</title>
-</head>
-<body>
-    <?php
+<h1><?php
     // FILEPATH: /workspace/phpdev/www/index.php
-    echo '<h1>Hello!</h1>';
-    // echo '<br>';
-    ?>
-</body>
-<h2>Such empty :(</h2>
-</html>
+    echo "Hello!\n";
+    echo "My name is Mitko";
+?></h1>
